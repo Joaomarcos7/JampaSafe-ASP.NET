fee51f7b9558891443cf35b70dd3b38d8a0fe000
João Marcos Chaves <102380570+Joaomarcos7@users.noreply.github.com>
2023-03-26 20:42:40 -0300

    Crud Inicial

---
+++ JampaSafe/Controllers/RelatosController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Dados;
+using JampaSafe.Models;
+
+
+namespace JampaSafe.Controllers
+{
+    public class RelatosController : Controller
+    {
+        private DadosEntities db = new DadosEntities();
+
+        public ActionResult Index() {
+            return View(db.Relato.ToList());
+        }
+
+        public ActionResult Create() {
+            ViewBag.codtipo = new SelectList(db.TipodeRelato,"codtipo","nome");
+            return PartialView("_Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idrelato,data,codtipo,descricao,bairro")] Relato relato, [Bind(Include = "imagem")] HttpPostedFileBase imagem)
+        {
+            if (imagem != null && imagem.ContentLength > 0) //VERIFICA SE O ARQUIVO FILE RECEBIDO NÃO É NULO E TB A SUA QTD DE BYTES
+            {
+                relato.imagem3= new byte[imagem.ContentLength];
+                using (var reader = new BinaryReader(imagem.InputStream))
+                {
+                    reader.Read(relato.imagem3, 0, imagem.ContentLength);
+
+                }
+                if (ModelState.IsValid)
+                {
+
+                    db.Relato.Add(relato);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+
+                ViewBag.codtipo = new SelectList(db.TipodeRelato, "codtipo", "nome");
+                return Content("Falha no envio do formulário!");
+
+            }
+        
+
+        public FileResult RetornaUrl(int id)
+        {
+            var model = db.Relato.Find(id);
+            return File(model.imagem3, "image/jpeg"); //qual url isso vai gerar no src??
+        }
+        [HttpPost]
+        public ActionResult Delete(int? id) {
+
+
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (db.Relato.Find(id)==null) {
+                return HttpNotFound();
+            }
+            var relato = db.Relato.Find(id);
+            db.Relato.Remove(relato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Edit(int? id)   //o ? indica que o parametro é opcional, ou seja, ele pode atribuir um valor Nulo
+        {
+
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            if (db.Relato.Find(id) == null)
+            {
+                return HttpNotFound();
+            }
+            var relato = db.Relato.Find(id);
+            return PartialView("_Edit",relato);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idrelato,data,codtipo,descricao,bairro")] Relato relato) {
+
+
+            if (ModelState.IsValid)
+            {
+                db.Entry(relato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return Content("ERRO NA ATUALIZAÇÃO DO RELATO");
+        }
+
+        protected override void Dispose(bool disposing) //POLIFORMISMO DO METODO DISPOSE DA CLASSE CONTROLLER HERDADA
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing); //BASE SE REFERE AO QUE ACONTECE NA CLASSE HERDADA!
+        }
+    }
+}
---
+++ JampaSafe/Models/Imagem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JampaSafe.Models
+{
+    public class Imagem
+    {
+
+        public string tipoimagem { get; set; }
+        public Imagem(string type) {
+            tipoimagem = type;
+        }
+    }
+}
